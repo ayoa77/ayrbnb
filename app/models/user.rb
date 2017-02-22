07682188649400362462bc242e298777ed315baf
ayoa77 <ayodeleamadi@gmail.com>
2017-02-22 12:43:33 +0800

    added facebook authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ApplicationRecord
-  include Clearance::User
+    has_many :authentications, dependent: :destroy
+    include Clearance::User
 
+    def self.authenticate(username, password)
+        if user = User.find_by(username: username)
+            user if password.present? && user.authenticated?(password)
+        end
+      end
+
+    def password_optional?
+        true
+    end
+
+    def self.create_with_auth_and_hash(authentication, auth_hash)
+        user = User.create!(username: auth_hash['extra']['raw_info']['name'], email: auth_hash['extra']['raw_info']['email'])
+        user.authentications << authentication
+        user
+    end
+
+    def fb_token
+        x = authentications.where(provider: :facebook).first
+        return x.token unless x.nil?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220032817) do
+ActiveRecord::Schema.define(version: 20170221111626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+  create_table "authentications", force: :cascade do |t|
+    t.string   "uid"
+    t.string   "token"
+    t.string   "provider"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentications_on_user_id", using: :btree
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "username",                       null: false
+    t.string   "email",                          null: false
+    t.string   "encrypted_password", limit: 128
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128, null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
+  end
+
+  add_foreign_key "authentications", "users"
 end
